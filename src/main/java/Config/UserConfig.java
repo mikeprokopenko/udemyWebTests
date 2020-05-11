24905289bb1a4f5f2f1411957220d029e932dd1d
Mike Prokopenko <mike.prokopenko@ekreative.com>
2020-05-11 17:34:02 +0300

    creating the config file

--- src/main/java/Config/UserConfig.java
+++ src/main/java/Config/UserConfig.java
@@ -2,7 +2,7 @@ package Config;
 
 public class UserConfig {
 
-    public static String userName = "MikeUdemyUser";
-    public static String userEmail = "misha.test.udemy@gmail.com";
+    public static String userName = "MikeTestUser";
+    public static String userEmail = "miketestemail@yopmail.com";
     public static String userPassword = "TestPassword1234!";
 }
