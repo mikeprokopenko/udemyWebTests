5d463403b4af8224d9d50b1148cca13bc1716b6b
Mike Prokopenko <mike.prokopenko@ekreative.com>
2020-05-12 10:06:45 +0300

    removed the config txt file, updating the Config class

--- src/main/java/Config/UserConfig.java
+++ src/main/java/Config/UserConfig.java
@@ -1,8 +1,12 @@
 package Config;
 
+import java.io.File;
+
 public class UserConfig {
 
-    public static String userName = "MikeTestUser";
-    public static String userEmail = "miketestemail@yopmail.com";
+    File file = new File("/config.txt");
+
+    public static String userName = "mikeUser" + System.currentTimeMillis();
+    public static String userEmail = System.currentTimeMillis() + "email@yopmail.com";
     public static String userPassword = "TestPassword1234!";
 }
