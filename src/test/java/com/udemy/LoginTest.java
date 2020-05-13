4e27ce7a09d843911cdc217e0e7214cf0e869fc0
Mike Prokopenko <mike.prokopenko@ekreative.com>
2020-05-13 16:42:05 +0300

    Updated LoginTest and TestBase classes, tried to add some options for browser

--- src/test/java/com/udemy/LoginTest.java
+++ src/test/java/com/udemy/LoginTest.java
@@ -4,6 +4,7 @@ import Config.UserConfig;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
@@ -12,6 +13,13 @@ public class LoginTest extends TestBase {
     public void loginWithValidCredentials() {
 
         By loginPopupButtonLocator = By.xpath("//button[@data-purpose='header-login']");
+        By loginPopupHeaderLocator = By.xpath("//div[@class='loginbox-v4__header']");
+        By emailForLoginFieldLocator = By.id("email--1");
+        By passwordForLoginFieldLocator = By.id("id_password");
+        By loginButtonInPopupLocator = By.id("submit-id-submit");
+        By profileButtonLocator = By.id("header.profile");
+        By usernameFieldInProfileLocator = By.xpath("//hgroup[@class='tooltip-container']/h2");
+        By logoutButtonInProfileLocator = By.xpath("//a[@data-purpose='do-logout']");
 
         driver.get("https://www.udemy.com/");
 
@@ -19,8 +27,38 @@ public class LoginTest extends TestBase {
         WebElement loginPopupButton = driver.findElement(loginPopupButtonLocator);
         loginPopupButton.click();
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginPopupHeaderLocator));
+        WebElement loginPopupHeader = driver.findElement(loginPopupHeaderLocator);
+        Assert.assertEquals(loginPopupHeader.getText(), "Войдите в учетную запись Udemy!");
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginButtonInPopupLocator));
+        WebElement emailForLoginField = driver.findElement(emailForLoginFieldLocator);
+        emailForLoginField.clear();
+        emailForLoginField.sendKeys(UserConfig.userEmailForLogin);
 
+        WebElement passwordForLoginField = driver.findElement(passwordForLoginFieldLocator);
+        passwordForLoginField.clear();
+        passwordForLoginField.sendKeys(UserConfig.userPasswordForLogin);
+
+        wait.until(ExpectedConditions.elementToBeClickable(loginPopupButtonLocator));
+        WebElement loginButtonInPopup = driver.findElement(loginButtonInPopupLocator);
+        loginButtonInPopup.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(profileButtonLocator));
+        WebElement profileButton = driver.findElement(profileButtonLocator);
+        profileButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameFieldInProfileLocator));
+        WebElement usernameFieldInProfile = driver.findElement(usernameFieldInProfileLocator);
+        Assert.assertEquals(usernameFieldInProfile.getText(), UserConfig.userNameForLogin);
+
+        WebElement logoutButtonInProfile = driver.findElement(logoutButtonInProfileLocator);
+        logoutButtonInProfile.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginPopupButtonLocator));
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.udemy.com/logout/");
+        loginPopupButton = driver.findElement(loginPopupButtonLocator);
+        Assert.assertEquals(loginPopupButton.getText(), "Log In");
     }
 
 }
--- src/test/java/com/udemy/TestBase.java
+++ src/test/java/com/udemy/TestBase.java
@@ -3,6 +3,7 @@ package com.udemy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -12,10 +13,12 @@ import java.io.File;
 public class TestBase {
 
     File chromeDriver = new File("src/main/resources/chromedriver.exe");
-    ChromeDriverService chromeService = new ChromeDriverService.Builder()
+
+        ChromeDriverService chromeService = new ChromeDriverService.Builder()
             .usingDriverExecutable(chromeDriver)
             .usingAnyFreePort()
             .build();
+
 
     WebDriver driver;
     WebDriverWait wait;
@@ -23,7 +26,12 @@ public class TestBase {
     @BeforeTest(description = "Open the browser")
     public void beforeMethod() {
 
+        ChromeOptions options = new ChromeOptions();
+
         driver = new ChromeDriver(chromeService);
+
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
 
         wait = new WebDriverWait(driver, 5);
     }
@@ -31,6 +39,7 @@ public class TestBase {
     @AfterTest (description = "Closing the browser")
     public void afterMethod() {
 
+        driver.manage().deleteAllCookies();
         driver.quit();
     }
 
